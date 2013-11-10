6fbec76104201d5e3fe824b10b3440470b8e8aab
Tom Whipple <mail@tomwhipple.com>
2013-11-10 09:45:11 -0800

    test README steps

--- UNNetPGP Tests/UNNetPGP_Tests.m
+++ UNNetPGP Tests/UNNetPGP_Tests.m
@@ -10,13 +10,21 @@
 
 #import "UNNetPGP.h"
 
-#define PLAINTEXT @"Plaintext: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit justo eros."
+#define PLAINTEXT @"Plaintext: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit justo eros.\n"
+
+NSString* getUUID(void){
+  CFUUIDRef theUUID = CFUUIDCreate(NULL);
+  CFStringRef string = CFUUIDCreateString(NULL, theUUID);
+  CFRelease(theUUID);
+  return (__bridge NSString *)string;
+}
 
 @interface UNNetPGP_Tests : XCTestCase {
 
   UNNetPGP* pgp;
   NSFileManager* fm;
   NSString* plaintextFile;
+  NSString* tmpDir;
 }
 
 @end
@@ -41,7 +49,15 @@
     XCTAssertTrue(error == nil, @"couldn't remove %@:\n%@", itemPath, error.localizedDescription);
   }
   
-  plaintextFile = [pgp.homeDirectory stringByAppendingPathComponent:@"plain.txt"];
+  NSString* newDir = [@"UUNetPGP_Tests" stringByAppendingPathComponent:getUUID()];
+  tmpDir = [NSTemporaryDirectory() stringByAppendingPathComponent:newDir];
+  
+  [fm createDirectoryAtPath:tmpDir withIntermediateDirectories:YES attributes:nil error:&error];
+  if (![fm fileExistsAtPath:tmpDir] || error) {
+    XCTFail(@"couldn't create tmpDir: %@", error.localizedDescription);
+  }
+
+  plaintextFile = [tmpDir stringByAppendingPathComponent:@"plain.txt"];
   
   NSData* plainData = [PLAINTEXT dataUsingEncoding:NSASCIIStringEncoding];
   [plainData writeToFile:plaintextFile atomically:YES];
@@ -68,6 +84,36 @@
   XCTAssertTrue(generated, @"key generation should be true");
 }
 
+- (void)testReadme
+{
+  pgp.userId = @"alice@resturant.org";
+  pgp.password = @"take out the garbage";
+  
+  // optinal
+//  pgp.publicKeyRingPath = [tmpdir stringByAppendingPathComponent:@"pubring.gpg"];
+//  pgp.secretKeyRingPath = [tmpdir stringByAppendingPathComponent:@"secring.gpg"];
+  
+  [pgp generateKey:1024];
+  
+  NSString* encryptedFile = [tmpDir stringByAppendingPathComponent:@"cyphertext.dat"];
+  BOOL success = NO;
+  success = [pgp encryptFileAtPath:plaintextFile toFileAtPath:encryptedFile];
+  XCTAssertTrue(success, @"encryption should report success");
+  XCTAssertTrue([fm fileExistsAtPath:encryptedFile], @"cyphertext file should exist: %@", encryptedFile);
+  
+  NSString* decryptedFile = [tmpDir stringByAppendingPathComponent:@"decrypted.txt"];
+  
+  //  HANGS: infinate loop.
+  success = [pgp decryptFileAtPath:encryptedFile toFileAtPath:decryptedFile];
+  XCTAssertTrue(success, @"decryption should report success");
+  XCTAssertTrue([fm fileExistsAtPath:decryptedFile], @"decrypted file should exist: %@", decryptedFile);
+  
+  NSError* error = nil;
+  NSString* decrypted = [NSString stringWithContentsOfFile:decryptedFile usedEncoding:nil error:&error];
+  XCTAssertNil(error, @"don't expect an error: %@", error.localizedDescription);
+  XCTAssertEqualObjects(PLAINTEXT, decrypted, @"expect decrypted text to match original plaintext");
+}
+
 - (void)testGenerateAndExportNamedKey {
   BOOL generated = [pgp generateKey:2048 named:@"alice" toDirectory:pgp.homeDirectory];
   XCTAssertTrue(generated, @"key generation for Alice should be true");
@@ -87,6 +133,8 @@
   
   NSData* inData = [PLAINTEXT dataUsingEncoding:NSASCIIStringEncoding];
   NSData* signedData = [pgp signData:inData];
+  
+  // FAILS: signedData is nil
   XCTAssertNotNil(signedData, @"expect signed data");
   
   success = [pgp verifyData:signedData];
@@ -102,6 +150,8 @@
   NSString* sigFile = [pgp.homeDirectory stringByAppendingPathComponent:@"signed.asc"];
   XCTAssertFalse([fm fileExistsAtPath:sigFile], @"don't expect a signature yet");
   success = [pgp signFileAtPath:plaintextFile writeSignatureToFile:sigFile detached:YES];
+  
+  // FAILS: returns false & doesn't create signed file.
   XCTAssertTrue(success, @"expect successful signing");
   XCTAssertTrue([fm fileExistsAtPath:sigFile], @"expect signature file %@", sigFile);
   
